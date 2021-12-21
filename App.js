8960969c5566dd36de9373e94eb969c5ce74c6c5
Hemit Patel <hemitpatel@computer4u.com>
2021-12-21 13:28:12 -0500

    initail

--- App.js
+++ App.js
@@ -1,20 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import moment from 'moment';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
+import Constants from 'expo-constants';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+ 
 export default function App() {
+  let now = moment().format('MMM Do YYYY, h:mm a');
+ 
+  const [status, setStatus] = useState('');
+  const [load, setLoad] = useState('');
+  const setPass = () => setStatus('you pass! ' + now);
+  const setFail = () => setStatus('you fail ' + now);
+ 
+  const getData = async () => {
+    try {
+      var value = await AsyncStorage.getItem('results');
+      if (value !== null) {
+        setLoad(value);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+ 
+  const storeData = async () => {
+    try {
+      await AsyncStorage.setItem('results', status);
+      alert('saved');
+      getData()
+    } catch (e) {
+      console.log(e);
+    }
+  };
+ 
+ 
+ 
+  useEffect(() => {
+    getData();
+  });
+ 
+  const deleteData = async () => {
+    try {
+      if (load != null) {
+      var value = await AsyncStorage.removeItem('results');
+      setLoad(value)
+      setStatus('')
+      alert('deleted data');
+      } else {
+        alert('error, there is nothing to reset.')
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>COVID-19 SYMPTOMS</Text>
+      <Text> </Text>
+      <Text>cough</Text>
+      <Text>dry throat </Text>
+      <Text>difficulty breathing</Text>
+      <Text></Text>
+      <Text>Press on the apporiate option!</Text>
+      <Text>
+        Also make sure to press Save or else the results will not come!
+      </Text>
+      <Text> </Text>
+      <TouchableOpacity onPress={setPass}>
+        <Text>I do not have any of these symptoms!</Text>
+      </TouchableOpacity>
+      <Text> </Text>
+      <TouchableOpacity onPress={setFail}>
+        <Text>I do have at least one of these symptoms!</Text>
+      </TouchableOpacity>
+      <Text> </Text>
+      <Text> </Text>
+      <Button title="Save" onPress={storeData}></Button>
+      <Button title="Reset results!" onPress={deleteData}></Button>
+ 
+      <Text>{load}</Text>
     </View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
 });
+ 
